e221635197d6ddb209c4a2263309b1424969cb6d
Danish Hussain <danishd.ug21.cs@nitp.ac.in>
2024-12-22 15:02:05 +0530

    Set Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,38 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import './App.css'
 import authService from "./appwrite/auth"
 import {login, logout} from "./store/authSlice"
-
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
   useEffect(() => {
     authService.getCurrentUser()
-    .then(userData => {
-      if(userData) {
-        dispatch(login({userData}));
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
       } else {
-        dispatch(logout());
+        dispatch(logout())
       }
-  })
-    .finally(() => setLoading(false));
-  }, []);
-  if(loading) {
-    return <div>Loading...</div>
-  }
-  return (
-    <div className="min-h-screen flex flex-wrap content-between">
-    <div className='w-full block'>
-      <Header />
-      <main>
-        TODO: {/* TODO: <outlet /> */}
-      </main>
-      <Footer />
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
     </div>
-        <h1>MegaBlog</h1>
-    </div>
-  );
+  ) : null
 }
 
 export default App
